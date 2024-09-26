9ed1a1be3c570cdf1c9f3681e6e56bf403685701
Yashaswi Rai <113192333+YashaishV@users.noreply.github.com>
2024-09-26 23:45:50 +0530

    Update roboInCircle.js

--- jollyScripts/roboInCircle.js
+++ jollyScripts/roboInCircle.js
@@ -1,1 +1,37 @@
+/**
+ * Welcome to the Stanford Karel IDE.
+ * This is a free space for you to 
+ * write any Karel program you want.
+ **/
 
+
+function main(){
+   for1Circle();
+   for1Circle();
+   for1Circle();
+   for1Circle();
+   for1Circle();
+   for1Circle();
+   for1Circle();
+   for1Circle();
+   for1Circle();
+   for1Circle();
+   }
+ 
+function for1Circle(){
+   for1StraightLine();
+   turnLeft();
+   for1StraightLine();
+   turnLeft();
+   for1StraightLine();
+   turnLeft();
+   for1StraightLine();
+   turnLeft();
+   }
+
+function for1StraightLine(){
+   move();
+   move();
+   move();
+   move();
+   } 
