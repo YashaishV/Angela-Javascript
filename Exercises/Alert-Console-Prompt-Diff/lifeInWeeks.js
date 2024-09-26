d98a838b0430284145ee13a3667450da021326a2
Yashaswi Rai <113192333+YashaishV@users.noreply.github.com>
2024-09-27 00:06:05 +0530

    Update lifeInWeeks.js

--- Exercises/Alert-Console-Prompt-Diff/lifeInWeeks.js
+++ Exercises/Alert-Console-Prompt-Diff/lifeInWeeks.js
@@ -1,1 +1,19 @@
+//life in weeks solution
 
+// lets take age Input
+var age = prompt("tell me your fucking age?");
+
+// subtract it by  90
+var remainingYears = (90 - age);
+
+// remainder is the remaining years to live 
+var months = remainingYears * 12;
+var weeks =  remainingYears * 52;
+var days = remainingYears * 365;
+
+//DOUBT: why used console.log instead of alert or prompt 
+console.log("your remaining life is " + remainingYears + " years, " + months + " months, " +  weeks + " weeks, "  + days + " days.");
+
+// try running the below two and see the difference
+// alert("your remaining life is " + remainingYears + " years, " + months + " months, " +  weeks + " weeks, "  + days + " days.");
+// prompt("your remaining life is " + remainingYears + " years, " + months + " months, " +  weeks + " weeks, "  + days + " days.");
